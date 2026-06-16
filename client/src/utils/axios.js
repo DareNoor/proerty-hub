72c248be110e9bdaa217f3f6da88bf543b996f15
DareNoor <noordare0@gmail.com>
2026-06-16 12:35:21 +0500

    Update axios baseURL to production backend

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL:'http://localhost:5000/api',
+  baseURL: 'https://proerty-hub-6nu2.vercel.app/api',
 })
 export default instance
